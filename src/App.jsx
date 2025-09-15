b48f6eaf152abb0fa6f2362f6630be6f84fe6289
tw30006 <original.heart.2016@gmail.com>
2025-09-15 22:15:05 +0800

    feat: url路徑判斷

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,23 @@
-import { Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
 
-import SearchResults from "./pages/SearchResults";
-import TrendingVideos from "./pages/TrendingVideos";
-import VideoDetail from "./pages/VideoDetail";
-import Channel from "./pages/Channel";
-import ChannelAnalytics from "./pages/ChannelAnalytics";
-import "./App.css";
-import { VideoContextProvider } from "./context/VideoDataContext";
+import SearchResults from './pages/SearchResults';
+// import TrendingVideos from './pages/TrendingVideos';
+import VideoDetail from './pages/VideoDetail';
+import Channel from './pages/Channel';
+import ChannelAnalytics from './pages/ChannelAnalytics';
+import './App.css';
+import { VideoContextProvider } from './context/VideoDataContext';
 
 function App() {
   return (
     <VideoContextProvider>
-      <div className="bg-gray-50 dark:bg-gray-900 text-gray-900 dark:text-white overflow-hidden">
+      <div className="min-h-screen bg-gray-50 dark:bg-gray-900 text-gray-900 dark:text-white">
         <Navbar />
         <main className="container mx-auto">
           <Routes>
             <Route path="/" element={<SearchResults />} />
-            <Route path="/trending" element={<TrendingVideos />} />
+            {/* <Route path="/trending" element={<TrendingVideos />} /> */}
             <Route path="/video/:id" element={<VideoDetail />}></Route>
             <Route path="/channel/:id" element={<Channel />}></Route>
             <Route
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,26 @@
-import { Link } from "react-router-dom";
-import ThemeToggle from "./ThemeToggle";
+import { Link,useLocation } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useVideoContext } from '../context/VideoDataContext';
+
 
 export default function Navbar() {
+  const { setSarchResults } = useVideoContext();
+  const location = useLocation();
+  useEffect(()=>{
+    if(location.pathname === '/'){
+      setSarchResults([]);
+    }
+  },[location.pathname])
+  
   return (
     <nav className="flex items-center justify-between px-4 py-2 bg-white dark:bg-gray-800 shadow-md">
       <div className="flex gap-4">
         <Link to="/" className="font-bold text-lg">
           YouTube Analytics
         </Link>
-        <Link to="/trending" className="hover:underline">
-          熱門影片
-        </Link>
+       
       </div>
-      <ThemeToggle />
+      
     </nav>
   );
 }
